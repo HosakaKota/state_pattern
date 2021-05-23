9d8848c0815e88cbdffe0b1ce2e9047f632e56c0
HosakaKota <71623933+HosakaKota@users.noreply.github.com>
2021-05-24 01:46:35 +0900

    Update src.cpp

--- src/src.cpp
+++ src/src.cpp
@@ -5,9 +5,9 @@ int main()
 {    
     enum State
     {
-        STATE_CREDIT
-        STATE_TITLE
-        STATE_OPENIG
+        STATE_CREDIT,
+        STATE_TITLE,
+        STATE_OPENIG,
         STATE_DEMO
     };
     
@@ -15,27 +15,27 @@ int main()
     
     switch(state_)
     {
-        case STATE_CREDIT
+        case STATE_CREDIT:
         {
             std::cout << "credit\n";
             std::this_thread::sleep_for(std::chrono::seconds(3));// 3 秒待つ        
             state_=STATE_TITLE;
             break;
         }
-        case STATE_TITLE
+        case STATE_TITLE:
         {
             std::cout << "title\n";
             std::this_thread::sleep_for(std::chrono::seconds(3));// 3 秒待つ
             state_=STATE_OPENING;
             break;
         }
-        case STATE_TITLE
+        case STATE_TITLE:
         {
             std::cout << "opening\n";
             std::this_thread::sleep_for(std::chrono::seconds(5));// 5 秒待つ
             state_=STATE_DEMO;
         }
-        case STATE_DEMO
+        case STATE_DEMO:
         {
             std::cout << "demo\n";
             std::this_thread::sleep_for(std::chrono::seconds(5));// 5 秒待つ
